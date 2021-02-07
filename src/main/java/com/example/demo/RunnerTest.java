014813617e811b920620c03174c0f9c10cc3e7a5
dell <dell@sid>
2021-02-07 13:06:21 +0530

    WHApp-997-new commit 2

--- src/main/java/com/example/demo/RunnerTest.java
+++ src/main/java/com/example/demo/RunnerTest.java
@@ -7,7 +7,9 @@ implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		System.out.println("RunnerTest.run():code modified");
+		int a =10;
+		System.out.println("a:"+a);
 		
 	}
 
