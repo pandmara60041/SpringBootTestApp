9a38d37275d0152b75fbc89c4629d9dccd0f8630
dell <dell@sid>
2021-02-07 12:38:44 +0530

    WHAPP-01/21-first time file

---
+++ src/main/java/com/example/demo/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class RunnerTest 
+implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
